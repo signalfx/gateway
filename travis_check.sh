d306ac522d8d5d4902e0210cb8e0eba776cf02b1
Jack <jack@signalfuse.com>
2014-10-15 15:21:36 -0700

    fix travis check grepping out expected no test package

--- travis_check.sh
+++ travis_check.sh
@@ -14,7 +14,7 @@ mdl README.md || exit 1
 go test -cover -covermode=atomic -v ./... || exit 1
 
 rm /tmp/no_100_coverage
-go test -cover -covermode=atomic ./... | grep -v "github.com/signalfuse/signalfxproxy\t" | grep -v "100.0% of statements" > /tmp/no_100_coverage
+go test -cover -covermode=atomic ./... | grep -v "github.com/signalfuse/signalfxproxy\t" | grep -v "github.com/signalfuse/signalfxproxy " | grep -v "100.0% of statements" > /tmp/no_100_coverage
 [[ ! -s /tmp/no_100_coverage ]] || cat /tmp/no_100_coverage
 [[ ! -s /tmp/no_100_coverage ]] || exit 1
 echo "OK!"
