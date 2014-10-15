3fd4d4333f7181efcf0f3c4ace0bf9b15efd57a5
Jack <jack@signalfuse.com>
2014-10-15 14:37:00 -0700

    Try changing travis check script

--- travis_check.sh
+++ travis_check.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 set -x
 rm -f /tmp/a || exit 1
-./format_all.sh > /tmp/a | die
+./format_all.sh > /tmp/a || exit 1
 [[ ! -s /tmp/a ]] || cat /tmp/a
 [[ ! -s /tmp/a ]] || exit 1
-./lint_all.sh > /tmp/a | die
+./lint_all.sh > /tmp/a || exit 1
 [[ ! -s /tmp/a ]] || cat /tmp/a
 [[ ! -s /tmp/a ]] || exit 1
-./vet_all.sh > /tmp/a | die
+./vet_all.sh > /tmp/a || exit 1
 [[ ! -s /tmp/a ]] || cat /tmp/a
 [[ ! -s /tmp/a ]] || exit 1
 mdl README.md || exit 1
-go test -cover -v ./... || exit 1
+go test -cover -covermode=atomic -v ./... || exit 1
 
 rm /tmp/no_100_coverage
-go test -cover ./... | grep -v "github.com/signalfuse/signalfxproxy\t" | grep -v "100.0% of statements" > /tmp/no_100_coverage
+go test -cover -covermode=atomic ./... | grep -v "github.com/signalfuse/signalfxproxy\t" | grep -v "100.0% of statements" > /tmp/no_100_coverage
 [[ ! -s /tmp/no_100_coverage ]] || cat /tmp/no_100_coverage
 [[ ! -s /tmp/no_100_coverage ]] || exit 1
 echo "OK!"
