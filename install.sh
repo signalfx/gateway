98451f2cb9822d36661f5af852a9f246359c9096
Jack Lindamood <jack@signalfuse.com>
2014-10-28 17:13:29 -0700

    Update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 mkdir -p /opt/sfproxy
 cd /opt/sfproxy
 yum install -y golang
 env GOPATH=`pwd` go get -u github.com/signalfuse/signalfxproxy
+ln -s /opt/sfproxy/src/github.com/signalfuse/signalfxproxy/signalfxproxy /etc/init.d/signalfxproxy
+if [ ! -f /opt/sfproxy/bin/signalfxproxy ]; then
+    echo "Unable to install proxy"
+    exit 1
+fi
+echo "Install ok!  To run execute '/etc/init.d/signalfxproxy start'"
