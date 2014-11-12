6d212c7caed01097d33ad7482d94540763c32e90
Jack Lindamood <jack@signalfuse.com>
2014-11-12 10:20:09 -0800

    Doc travis check

--- travis_check.sh
+++ travis_check.sh
@@ -1,27 +1,47 @@
 #!/bin/bash
 set -x
+#
+# ---- Check shell scripts
+# 
 shellcheck install.sh || exit 1
 shellcheck signalfxproxy || exit 1
 # Yo dawg
 shellcheck travis_check.sh || exit 1
+
+#
+# ---- Check markdown
+#
+mdl README.md || exit 1
+
+#
+# ---- Check JSON
+#
 set -e
 # Want example config file to be valid json
-python -m json.tool < exampleSfdbproxy.conf
+python -m json.tool < exampleSfdbproxy.conf > /dev/null
 set +e
 
-rm -f /tmp/a || exit 1
+#
+# ---- Check go syntax
+#
+rm -f /tmp/a /tmp/no_100_coverage || exit 1
 go install . || exit 1
-find . -type f | grep -v '.git' | grep '.go' | xargs -n1 -P8 go fmt > /tmp/a || exit 1
+find . -type f -name \*.go | grep -v '.git' | xargs -n1 -P8 go fmt > /tmp/a || exit 1
+# I want to print it out for debugging purposes, while still existing if exist
 [[ ! -s /tmp/a ]] || cat /tmp/a
 [[ ! -s /tmp/a ]] || exit 1
-find . -type f | grep -v ".git" | grep '.go' | xargs -n1 -P8 golint > /tmp/a || exit 1
+
+find . -type f -name \*.go | grep -v ".git" | xargs -n1 -P8 golint > /tmp/a || exit 1
 [[ ! -s /tmp/a ]] || cat /tmp/a
 [[ ! -s /tmp/a ]] || exit 1
-find . -type f | grep -v '.git' | grep '.go' | xargs -n1 -P8 go vet > /tmp/a || exit 1
+
+find . -type f -name \*.go | grep -v '.git' | xargs -n1 -P8 go vet > /tmp/a || exit 1
 [[ ! -s /tmp/a ]] || cat /tmp/a
 [[ ! -s /tmp/a ]] || exit 1
-mdl README.md || exit 1
-rm -f /tmp/no_100_coverage
+
+#
+# ---- Check for 100% code coverage
+#
 go test -cover -race -covermode=atomic -parallel=8 ./... | grep -v "100.0% of statements" > /tmp/no_100_coverage
 [[ ! -s /tmp/no_100_coverage ]] || cat /tmp/no_100_coverage
 [[ ! -s /tmp/no_100_coverage ]] || exit 1
