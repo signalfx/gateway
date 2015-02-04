3944848d10d3ca53a500a1b12b66a5c9f98fc306
Jack Lindamood <jack@signalfuse.com>
2015-02-04 00:45:08 -0800

    Fix linting

--- listener/negroni_test.go
+++ listener/negroni_test.go
@@ -1,9 +1,10 @@
 package listener
 
 import (
-	"testing"
-	"github.com/stretchr/testify/assert"
 	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestMetricTrackingMiddleware(t *testing.T) {
