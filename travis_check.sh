8c38b89a44952eaa80146a36814fbde8a4ece926
Jack Lindamood <jack@signalfuse.com>
2015-04-16 09:13:03 -0700

    Better doc travis check

--- travis_check.sh
+++ travis_check.sh
@@ -13,6 +13,7 @@ shellcheck travis_check.sh || exit 1
 # ---- Check markdown
 #
 # Note: there is one line (a curl) that we can't help but make long
+# Verify rules at https://github.com/mivok/markdownlint/blob/master/docs/RULES.md
 echo -e "# Ignore Header" > /tmp/ignore_header.md
 cat /tmp/ignore_header.md README.md | grep -av curl | grep -av 'Build Status' | mdl --warnings || exit 1
 
