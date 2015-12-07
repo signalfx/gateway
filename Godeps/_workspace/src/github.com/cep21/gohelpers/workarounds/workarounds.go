51efaa20effec8cb5083b49c74960bd9d8532099
Matthew Pound <mwp@signalfx.com>
2015-12-07 09:05:59 -0800

    update gohelpers and protobuf stuff

--- Godeps/_workspace/src/github.com/cep21/gohelpers/workarounds/workarounds.go
+++ Godeps/_workspace/src/github.com/cep21/gohelpers/workarounds/workarounds.go
@@ -17,22 +17,22 @@ func GolangDoesnotAllowPointerToUintLiteral(s uint32) *uint32 {
 	return &s
 }
 
-// GolangDoesnotAllowPointerToUintLiteral allows one to take the address of a uint32 literal
+// GolangDoesnotAllowPointerToFloat64Literal allows one to take the address of a float64 literal
 func GolangDoesnotAllowPointerToFloat64Literal(s float64) *float64 {
 	return &s
 }
 
-// GolangDoesnotAllowPointerToUintLiteral allows one to take the address of a uint32 literal
+// GolangDoesnotAllowPointerToIntLiteral allows one to take the address of a int64 literal
 func GolangDoesnotAllowPointerToIntLiteral(s int64) *int64 {
 	return &s
 }
 
-// GolangDoesnotAllowPointerToUintLiteral allows one to take the address of a uint32 literal
+// GolangDoesnotAllowPointerToUint16Literal allows one to take the address of a uint16 literal
 func GolangDoesnotAllowPointerToUint16Literal(s uint16) *uint16 {
 	return &s
 }
 
-// GolangDoesnotAllowPointerToUintLiteral allows one to take the address of a uint32 literal
-func GolangDoesnotAllowPointerToDurationLiteral(s time.Duration) *time.Duration {
+// GolangDoesnotAllowPointerToBooleanLiteral allows one to take the address of a bool literal
+func GolangDoesnotAllowPointerToBooleanLiteral(s bool) *bool {
 	return &s
 }
