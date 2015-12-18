9427e77c99f8677f066b9049635c61eaaf096afb
Jack Lindamood <jack@signalfx.com>
2015-12-18 13:57:53 -0800

    Update generator

--- Godeps/_workspace/src/github.com/signalfx/golib/datapoint/dptest/generator.go
+++ Godeps/_workspace/src/github.com/signalfx/golib/datapoint/dptest/generator.go
@@ -28,7 +28,7 @@ func init() {
 	globalSource.Dims = map[string]string{"source": "randtest"}
 	globalSource.Dptype = datapoint.Gauge
 	globalSource.TimeSource = time.Now
-	globalEventSource.EventType = "imanotify.notify_type"
+	globalEventSource.EventType = "imanotify.notify_instance"
 	globalEventSource.Dims = map[string]string{"host": "mwp-signalbox", "plugin": "my_plugin", "f": "x", "plugin_instance": "my_plugin_instance", "k": "v"}
 	globalEventSource.Meta = map[string]interface{}{"string": "value", "boolean": true, "int": int64(40), "double": 0.0, "": "nothing"}
 	globalEventSource.TimeSource = time.Now
