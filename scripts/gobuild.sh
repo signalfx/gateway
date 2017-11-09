4b753bbbefce625d1794b9cb72d3dd2606302407
Matthew Pound <mwp@signalfx.com>
2017-11-09 14:55:45 -0800

    Fix govendor wrt to prometheus

--- scripts/gobuild.sh
+++ scripts/gobuild.sh
@@ -6,7 +6,7 @@ set -o pipefail
 # Make sure all vendor/test libs are installed so that vet works properly
 (cd vendor && find . -type d -exec bash -c '(cd "$0" && if [ -n "$(ls -A *.go 2>/dev/null)" ]; then go install; fi)' {} \;)
 
-x=$(gobuild list | grep -v ^vendor)
+x=$(gobuild list)
 for y in $x; do
   gobuild check "$y" 2>&1
 done
