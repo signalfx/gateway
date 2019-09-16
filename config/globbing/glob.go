f29316ae055a5c7b27c3420b8ee949ec867cb4b4
charliesignalfx <charlie@signalfx.com>
2019-09-16 11:19:53 -0400

    refactor getGlob to be exported in new package under config/globbing

---
+++ config/globbing/glob.go
@@ -0,0 +1,15 @@
+package globbing
+
+import (
+	"github.com/gobwas/glob"
+	"strings"
+)
+
+// GetGlob takes a config and returns a gobwas Glob that has all other glob/regex operators escaped besides "*".
+func GetGlob(pattern string) glob.Glob {
+	patternParts := strings.Split(pattern, "*")
+	for i := 0; i < len(patternParts); i++ {
+		patternParts[i] = glob.QuoteMeta(patternParts[i])
+	}
+	return glob.MustCompile(strings.Join(patternParts, "*"))
+}
